66d8ceb886170263ce0026c7e5c1240707a76fd4
みやはら <myhr.atk@gmail.com>
2026-05-03 10:36:24 +0900

    Update 03-illust-ichiran.js

--- 01_JavaScript/03-illust-ichiran.js
+++ 01_JavaScript/03-illust-ichiran.js
@@ -3,6 +3,7 @@ fetch('../03_JSON/illust.json')
     .then(data => {
         const gallery = document.getElementById('thumbnail');
 
+
         const artsReversed = data.arts.slice().reverse();
 
         artsReversed.forEach(art => {
@@ -19,14 +20,20 @@ fetch('../03_JSON/illust.json')
             img.src = src;
             img.alt = `art-${art.id}`;
 
+
             img.addEventListener('click', () => {
-                location.href = `04-illust-kobetsu page.html?id=${art.id}`;
+                location.href = `04-illust-kobetsu-page.html?id=${art.id}`;
             });
 
+
             img.addEventListener('dragstart', e => e.preventDefault());
+
+
             img.addEventListener('contextmenu', e => e.preventDefault());
 
+
             box.appendChild(img);
+            
             gallery.appendChild(box);
         });
     })
